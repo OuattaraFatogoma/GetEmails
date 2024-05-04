92aaed086818057695bccc23b543d554a5fa5ae6
OuattaraFatogoma <ouattarafatogmaabdoulaziz@gmail.com>
2024-05-04 12:54:55 +0100

    correct a bug

--- bot.js
+++ bot.js
@@ -15,12 +15,13 @@ const bot = async (req, res) => {
         executablePath: process.env.NODE_ENV === "production"
             ? process.env.PUPPETEER_EXECUTABLE_PATH
             : puppeteer.executablePath(),
-        headless: true,
+        headless: false
+        ,
     });
+    const page = await browser.newPage();
 
     try {
         //Open browser and search the toSearch
-        const page = await browser.newPage();
         await page.goto("https://www.google.com/");
         await page.waitForSelector(".gLFyf");
         await page.type(".gLFyf", toSearch , {delay: 10});
@@ -38,23 +39,28 @@ const bot = async (req, res) => {
             } 
         }
 
-        // Collect each html element that can contain email
-        const elements = await page.$$('div.MjjYud');
-        for(let element of elements){
-            let source = await page.evaluate(el => el.querySelector("div").textContent, element);
-            const matches = source.match(regex);
-            if(matches!==null) emails.push(...matches);
-        };
- 
-        // clean emails and send them to the frontend
-        emails = [...(new Set(emails))];
-        emails = emails.map(email => email.toLowerCase());
-        res.status(200).json(emails);
     } catch (error) {
         console.log(error);
-        res.status(500).json('Something want wrong please try again');
     }
     finally{
+        try {
+            // Collect each html element that can contain email
+            const elements = await page.$$('div.MjjYud');
+            for(let element of elements){
+                let source = await page.evaluate(el => el.textContent, element);
+                await element.dispose();
+                const matches = source.match(regex);
+                if(matches!==null) emails.push(...matches);
+            };
+     
+            // clean emails and send them to the frontend
+            emails = [...(new Set(emails))];
+            emails = emails.map(email => email.toLowerCase());
+            res.status(200).json(emails);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json('Something want wrong please try again');
+        }
         await browser.close();
     }
 }
@@ -70,9 +76,9 @@ async function autoscroll(page){
         previousHeight = await page.evaluate('document.body.scrollHeight');
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
         await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-        await new Promise((resolve)=> setTimeout(resolve,10));
+        await new Promise((resolve)=> setTimeout(resolve,20));
         if(page.waitForSelector(".RVQdVd")){
-          await new Promise((resolve)=> setTimeout(resolve,10));
+          await new Promise((resolve)=> setTimeout(resolve,20));
           await page.click(".RVQdVd")
           break;
         } 
